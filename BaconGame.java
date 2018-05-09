3440730c24e219037b148f0e12b520d973e05e9d
Eitan <vilker@Eitans-MacBook-Air.local>
2018-05-09 17:08:45 -0400

    Worked on Bacon number method

--- BaconGame.java
+++ BaconGame.java
@@ -1,8 +1,31 @@
+import java.util.*;
+
 
 public class BaconGame {
 	
+	public class DistanceComparator implements Comparator<Actor> {
+		public int compare(Actor a, Actor b) {
+			return a.getDistance() - b.getDistance();
+		}
+	}
 	
-	Graph<String, Integer> baconGame = new AdjacencyMapGraph<String, Integer>();
+	public int findBaconNumber(Graph<Actor, Integer> theGraph, Actor chosen, Actor goal) {
+		Actor current = chosen;
+		Iterator<Actor> iterator = theGraph.outNeighbors(current).iterator(); 
+		PriorityQueue<Actor> theQueue = new PriorityQueue<Actor>();
+		
+		theQueue.add(chosen);
+		while(theQueue.size() > 0)
+			
+		
+//		while(iterator.hasNext()) {
+//			Actor actor = iterator.next();
+//			if(actor.equals(goal)) {
+//				return actor.getDistance();
+//			}	
+//		}
+		return -1;
+
+	}
 	
-	Iterator<String> iterator = baconGame.outNeighbors(current).iterator(); 
 }
