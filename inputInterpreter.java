16a1de196e13e355730438ac22c3fb8f5d3a2b20
alowverus <aidan.low98@gmail.com>
2018-05-09 16:26:13 -0400

    blah

--- inputInterpreter.java
+++ inputInterpreter.java
@@ -1,15 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class inputInterpreter {
 
 	public static void main(String args[]) {
 		try {
 			BufferedReader file = new BufferedReader(new FileReader("inputs/ps4/actors.txt"));
+			String line;
+			while ((line = file.readLine()) != null) {
+				line.split("\\|");
+			}
 		}
 		catch (FileNotFoundException e) {
-			System.out.println("Blah");
+			System.out.println("File not found.");
+		}
+		catch (IOException e) {
+			System.out.println("Index out of bounds.");
 		}
 	}
 }
