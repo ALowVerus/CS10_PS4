1fbf2c092d924ec32e550ad264fefa6c667005d6
alowverus <aidan.low98@gmail.com>
2018-05-09 16:17:34 -0400

    fixed import

--- inputInterpreter.java
+++ inputInterpreter.java
@@ -3,10 +3,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class inputInterpreter {
-	public static void main(String args[]) throws FileNotFoundException {
-		BufferedReader file = new BufferedReader(new FileReader("inputs/ps4/actors.txt"));
-		
-		
-		
+
+	public static void main(String args[]) {
+		try {
+			BufferedReader file = new BufferedReader(new FileReader("inputs/ps4/actors.txt"));
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Blah");
+		}
 	}
 }
