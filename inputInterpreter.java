21913333443b3444af41e2ec76e50b4b79d21d99
Eitan <vilker@Eitans-MacBook-Air.local>
2018-05-09 16:10:05 -0400

    Added throws

--- inputInterpreter.java
+++ inputInterpreter.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class inputInterpreter {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws FileNotFoundException {
 		BufferedReader file = new BufferedReader(new FileReader("inputs/ps4/actors.txt"));
+		
+		
+		
 	}
 }
