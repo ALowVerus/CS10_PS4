73cc53bad41f783af423ed051e8a8550a09eacaa
alowverus <aidan.low98@gmail.com>
2018-05-09 16:04:38 -0400

    blah

---
+++ inputInterpreter.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class inputInterpreter {
+	public static void main(String args[]) {
+		BufferedReader file = new BufferedReader(new FileReader("inputs/ps4/actors.txt"));
+	}
+}
