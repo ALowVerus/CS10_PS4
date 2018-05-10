bd22fa7eba46d4f212aad1b143af5f0d45c5ccb8
alowverus <aidan.low98@gmail.com>
2018-05-10 00:25:46 -0400

    Working on the Bacon game

--- Actor.java
+++ Actor.java
@@ -5,43 +5,19 @@ public class Actor {
 	private String name;
 	private boolean visited = false;
 	private ArrayList<Actor> path;
-	private ArrayList<Actor> fellowActors;
 	private int distance = infinity;
 
-	public Actor(String name, ArrayList<Actor> fellows) {
+	public Actor(String name) {
 		this.name = name;
-		this.fellowActors = fellows;
 	}
 	
-	public void setVisited(boolean visited) {
-		this.visited = visited;
-	}
+	// Getters
+	public boolean getVisited() {return visited;}
+	public ArrayList<Actor> getPath(){return path;}
+	public int getDistance() {return distance;}
 	
-	public boolean getVisited() {
-		return visited;
-	}
-	
-	public void setPath(ArrayList<Actor> path) {
-		this.path = path;
-	}
-	
-	public ArrayList<Actor> getPath(){
-		return path;
-	}
-	
-	public void setFellowActors(ArrayList<Actor> fellows) {
-		this.fellowActors = fellows;
-	}
-	
-	public ArrayList<Actor> getFellowActors(){
-		return fellowActors;
-	}
-	
-	public void setDistance(int distance) {
-		this.distance = distance;
-	}
-	
-	public int getDistance() {
-		return distance;
-	}
+	// Setters
+	public void setVisited(boolean visited) {this.visited = visited;}
+	public void setPath(ArrayList<Actor> path) {this.path = path;}
+	public void setDistance(int distance) {this.distance = distance;}
 }
--- BaconGame.java
+++ BaconGame.java
@@ -1,4 +1,7 @@
+import java.io.*;
 import java.util.*;
+
+import sun.misc.VM;
 
 
 public class BaconGame {
@@ -7,6 +10,44 @@ public class BaconGame {
 		public int compare(Actor a, Actor b) {
 			return a.getDistance() - b.getDistance();
 		}
+	}
+	
+	public static BufferedReader buffer(String fileAddress) {
+		TreeMap thisMap = new TreeMap();
+		try {
+			return new BufferedReader(new FileReader(fileAddress));
+		}
+		catch (FileNotFoundException e) { System.out.println("File not found."); }
+		catch (IOException e) { System.out.println("Index out of bounds.");	}
+		return null;
+	}
+	
+	public static TreeMap<String, String> makeMap(String fileAddress) {
+		BufferedReader file = buffer(fileAddress);
+		TreeMap<String, String> thisMap = new TreeMap();
+		String line;
+		try {
+			while ((line = file.readLine()) != null) {
+				String[] thisLine = line.split("\\|");
+				thisMap.put(thisLine[0], thisLine[1]);
+			}
+		}
+		catch (IOException e){ System.out.println("Line failed to read."); }
+		return thisMap;
+	}
+	
+	
+	public static <V,E> Graph<V,E> bfs(Graph<V,E> g, V source) {
+		
+	}
+	public static <V,E> List<V> getPath(Graph<V,E> tree, V v) {
+		
+	}
+	public static <V,E> Set<V> missingVertices(Graph<V,E> graph, Graph<V,E> subgraph) {
+		
+	}
+	public static <V,E> double averageSeparation(Graph<V,E> tree, V root) {
+		
 	}
 	
 	public int findBaconNumber(Graph<Actor, Integer> theGraph, Actor chosen, Actor goal) {
@@ -26,6 +67,43 @@ public class BaconGame {
 //		}
 		return -1;
 
+	}
+
+	public static void main(String args[]){
+		// Make referencable maps from the input files
+		TreeMap<String, String> actorMap = makeMap("inputs/ps4/actorsTest.txt");
+		TreeMap<String, String> movieMap = makeMap("inputs/ps4/moviesTest.txt");
+		
+		AdjacencyMapGraph thisGraph = new AdjacencyMapGraph();
+		for (Object actorKey : actorMap.keySet()) {thisGraph.insertVertex(new Actor(actorMap.get(actorKey)));}
+		for (Object movieKey : movieMap.keySet()) {thisGraph.insertVertex(movieMap.get(movieKey));}
+		
+		// Add links from actors to movie objects
+		BufferedReader lineFile = buffer("inputs/ps4/movie-actorsTest.txt"); String line;
+		try {
+			while ((line = lineFile.readLine()) != null ) {
+				String[] thisLine = line.split("\\|");
+				Object movie = movieMap.get(thisLine[0]);
+				Object actor = actorMap.get(thisLine[1]);
+				thisGraph.insertUndirected(actor, movie, "Bump");
+				System.out.println("link from " + movie + " to " + actor);
+			}
+		}
+		catch (IOException e) { System.out.println("Line failed to read."); }
+		
+		// Consolidate the movie objects, turning them into the names of direct links from actor to actor
+		for (Object movie : movieMap.keySet()) {
+			Iterable<VM> neighbors = thisGraph.inNeighbors(movie);
+			for (Object actor : neighbors) {
+				for (Object neighbor : neighbors) {
+					if (actor != neighbor) {
+						thisGraph.insertUndirected(actor, neighbor, movie);
+					}
+				}
+			}
+		}
+		
+		System.out.println("Blah");
 	}
 	
 }
